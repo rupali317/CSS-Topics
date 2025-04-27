a8fcafc87c1df91e8f7a5d4a078f0520dd262a23
rupali317 <rupali317@gmail.com>
2025-04-27 20:20:41 +0800

    Menu transition demo

--- Chapter 5 - Animations/main.js
+++ Chapter 5 - Animations/main.js
@@ -1,5 +1,11 @@
-const button = document.querySelector(".button-3");
+const button3 = document.querySelector(".button-3");
+const button4 = document.querySelector(".button-4");
+const nav = document.querySelector("nav");
 
-button.addEventListener("click", () => {
-  button.classList.add("disabled");
+button3.addEventListener("click", () => {
+  button3.classList.add("disabled");
+});
+
+button4.addEventListener("click", () => {
+  nav.classList.toggle("menu-open");
 });
