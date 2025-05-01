d9aadc4f7ba9f265002904cc1d62cce1e922abd6
rupali317 <rupali317@gmail.com>
2025-05-01 19:33:42 +0800

    Experimenting with having different animations for states like hover and focus

--- Chapter 5 - Animations/main.js
+++ Chapter 5 - Animations/main.js
@@ -1,6 +1,27 @@
+const button1 = document.querySelector(".button-1");
 const button3 = document.querySelector(".button-3");
 const button4 = document.querySelector(".button-4");
 const nav = document.querySelector("nav");
+
+button1.addEventListener("mouseenter", () => {
+  button1.classList.add("hovered");
+  button1.style.transition = "background-color 1s linear";
+});
+
+button1.addEventListener("mouseleave", () => {
+  button1.classList.remove("hovered");
+  button1.style.transition = "background-color 1s linear";
+});
+
+button1.addEventListener("focus", () => {
+  button1.classList.add("focused");
+  button1.style.transition = "background-color 10s linear";
+});
+
+button1.addEventListener("blur", () => {
+  button1.classList.remove("focused");
+  button1.style.transition = "background-color 10s linear";
+});
 
 button3.addEventListener("click", () => {
   button3.classList.add("disabled");
